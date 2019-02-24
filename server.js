d2e6225c4010de97d49050d0cd1f5e52282308ef
SheekhaJ <jariwals@oregonstate.edu>
2019-02-24 13:11:25 -0800

    Code to get sound names and urls from S3

--- server.js
+++ server.js
@@ -12,49 +12,58 @@ app.set('view engine', 'ejs');
 
 aws.config.loadFromPath('config.json');
 
-var corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+var credentials = new aws.SharedIniFileCredentials({ profile: 'default' });
+aws.config.credentials = credentials;
+var s3 = new aws.S3({ credentials });
 
-var s3 = new aws.S3({ accessKeyId: 'AKIAIYGRY4FCAO52RY6A', secretAccessKey: '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' })
 
-var soundsArray = [];
 
-app.get('/', cors(corsOptions), function (req, res) {
-    category = req.query.category ? req.query.category : 'funny';
-    console.log('category received on backend is ', category);
+app.get('/', function (req, res) {
+    console.log('current file: '+__filename);
+    console.log('current dir: '+__dirname);
+    
+    // for(const key in req.query){
+    //     console.log('key: '+key+' param:'+req.query[key]);
+    // }
 
-    // var soundsMap = {};
-    s3.listObjectsV2({
-        Bucket: 'gifovie-bucket',
-        Prefix: 'sounds/'+category
-    }, function (err, data) {
-        if (err) {
-            console.log('err: ' + err);
-        } else {
-            // var objects = data.Contents;
-            // console.log('in s3.listObjects! category is '+category);
-            for (i = 0; i < data.KeyCount; i++) {
-                var element = data.Contents[i];
-                var name = element.Key
-                if (name.includes('wav')) {
-                    console.log('key is ' + name);
-                    var url = s3.getSignedUrl('getObject', {
-                        Bucket: data.Name,
-                        Key: name
-                    });
-                    // soundsMap[i] = [name,url];
-                    // soundsMap[i].push()
-                    soundsArray.push(url);
+    // if (req.query.category) {
+        category = req.query.category ? req.query.category : 'funny';
+        console.log('category received on backend is ', category);
+        
+        var soundsArray = [];
+        // var soundsMap = new Map();
+        s3.listObjectsV2({
+            Bucket: 'gifovie-bucket',
+            Prefix: 'sounds/' + category
+        }, function (err, data) {
+            if (err) {
+                console.log('err in listObjects: ' + err);
+            } else {
+                // var objects = data.Contents;
+                // console.log('in s3.listObjects! category is '+category);
+                for (i = 0; i < data.KeyCount; i++) {
+                    var element = data.Contents[i];
+                    var name = element.Key
+                    if (name.includes('wav')) {
+                        // console.log('key is ' + name);
+                        var url = s3.getSignedUrl('getObject', {
+                            Bucket: data.Name,
+                            Key: name
+                        });
+                        soundsArray.push({ 'name': name, 'url': url });
+                    }
                 }
             }
-        }
-        console.log('\n sounds are '+soundsArray);
-        // return res.send(urls);
-        res.render('index', {sounds: soundsArray});
-    });
+            console.log('\n sounds are '+soundsArray);
+            res.render('index', { sounds: soundsArray });
+        });
+    // }
+
+    if(req.query.data) {
+        console.log('in backend data is ' + data);
+    }
 });
+
 
 app.listen(port, function () {
     console.log('App running on ' + port + ' port.');

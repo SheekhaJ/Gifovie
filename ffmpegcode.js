2dd3eeba9611e53583840aef358dbeeb70a0eb54
SheekhaJ <jariwals@oregonstate.edu>
2019-02-23 22:15:44 -0800

    Code to combine two mp4 files

--- ffmpegcode.js
+++ ffmpegcode.js
@@ -1,4 +1,5 @@
 const ff = require('fluent-ffmpeg');
+var fs = require('fs');
 
 // ff().
 // on('start', function(commandLine){
@@ -23,16 +24,32 @@ const ff = require('fluent-ffmpeg');
 //(echo file 'output.mp4' & echo file 'newoutput.mp4') > list.txt
 //ffmpeg -safe 0 -f concat -i list.txt -c copy final.mp4
 
-ff().
-on('start', function(commandLine){
-    console.log('combining mp4!');
-})
-.addInput('newoutput.mp4')
-.addInput('output.mp4')
-.on('end', function(){
-    console.log('Process ended!');
-})
-.on('error', function(err){
-    console.log('error occured: '+err);
-})
-.mergeToFile('./combined.mp4', './temp/');
+var fileList = ['output.mp4', 'newoutput.mp4'];
+var listFileNames = 'filelist.txt', fileNames = '';
+
+fileList.forEach(function(fileName, index){
+    console.log('filename: '+fileName);
+    fileNames = fileNames + ' file ' + "'" + fileName + '\'\r\n';
+});
+
+fs.writeFileSync(listFileNames, fileNames);
+
+var combine = ff().input(listFileNames)
+.inputOptions(['-f concat', '-safe 0'])
+.outputOptions('-c copy')
+.save('codeoutput.mp4');
+
+// ff().
+// on('start', function(commandLine){
+//     console.log('combining mp4!');
+// })
+// .concat()
+// .addInput('newoutput.mp4')
+// .addInput('output.mp4')
+// .on('end', function(){
+//     console.log('Process ended!');
+// })
+// .on('error', function(err){
+//     console.log('error occured: '+err);
+// })
+// .mergeToFile('./combined.mp4', './temp/');

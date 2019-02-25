808eb8ed1fb3e33b35f75f9b560567f7febb4979
SheekhaJ <jariwals@oregonstate.edu>
2019-02-24 18:37:21 -0800

    Added routes and partials

---
+++ routes/index.js
@@ -0,0 +1,56 @@
+'use strict';
+const aws = require('aws-sdk');
+
+module.exports = function (app) {
+
+    aws.config.credentials = credentials;
+    aws.config.loadFromPath('config.json');
+
+    var credentials = new aws.SharedIniFileCredentials({ profile: 'default' });
+    var s3 = new aws.S3({ credentials });
+
+    app.get('/', function (req, res) {
+        res.render('pages/welcome');
+    });
+
+    app.get('/addGIFs', function (req, res) {
+        res.render('pages/addGIFs');
+    });
+
+    app.get('/addSounds', function (req, res) {
+        var category = req.query.category ? req.query.category : 'funny';
+        console.log('category received on backend is ', category);
+
+        var soundsArray = [];
+        s3.listObjectsV2({
+            Bucket: 'gifovie-bucket',
+            Prefix: 'sounds/' + category
+        }, function (err, data) {
+            if (err) {
+                console.log('err in listObjects: ' + err);
+            } else {
+                // var objects = data.Contents;
+                // console.log('in s3.listObjects! category is '+category);
+                for (var i = 0; i < data.KeyCount; i++) {
+                    var element = data.Contents[i];
+                    var name = element.Key
+                    if (name.includes('wav')) {
+                        // console.log('key is ' + name);
+                        var url = s3.getSignedUrl('getObject', {
+                            Bucket: data.Name,
+                            Key: name
+                        });
+                        soundsArray.push({ 'name': name.replace('sounds/' + category + '/', '').replace('.wav', ''), 'url': url });
+                    }
+                }
+            }
+            console.log('\n sounds are ' + JSON.stringify(soundsArray));
+            res.render('pages/addSounds', { sounds: soundsArray, category: category });
+        });
+        // res.render('pages/addSounds', { sounds: soundsArray, category: category });
+    });
+
+    app.get('/reviewGIFovie', function (req, res) {
+        res.render('pages/reviewGIFovie');
+    });
+};

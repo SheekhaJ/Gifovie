76c0bd6a0402a7b7360cb8854707a50e9c11d97f
SheekhaJ <jariwals@oregonstate.edu>
2019-03-12 20:38:09 -0700

    Added promise to let sounds and gifs to merge first

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@
 const aws = require('aws-sdk');
 const ffmpeg = require('fluent-ffmpeg');
 const fs = require('fs');
+const https = require('https');
+const path = require('path');
 
 module.exports = function (app) {
 
@@ -12,6 +14,23 @@ module.exports = function (app) {
     var s3 = new aws.S3({ credentials });
 
     app.get('/', function (req, res) {
+        // var listFileNames = process.cwd() +"\\"+"list.txt", fileNames = "";
+        // console.log('file to store sub-gifovies: '+listFileNames);
+        
+        // // fileList.forEach(function(fileName, index){
+        // //     console.log("fileName: "+fileName);
+        // //     fileNames = fileNames + " file " + "'" + fileName + "'\r\n";
+        // // });
+
+        // // fs.writeFileSync(listFileNames, fileNames);
+        // console.log('path resolve: '+listFileNames);
+        // fs.readFile("C:\\Users\\Sheekha\\OneDrive\\OneDrive - Oregon State University\\workspace\\vs workspace\\Gifovie\\list.txt", 'utf8', function(err, data){
+        //     // console.log('inside readFileSync!');
+        //     if(err)
+        //         console.log('error is '+err);
+        //     else
+        //         console.log('content from resolved path '+listFileNames+' is '+data);
+        // });
         
         res.render('pages/welcome');
     });
@@ -34,7 +53,7 @@ module.exports = function (app) {
             } else {
                 // var objects = data.Contents;
                 // console.log('in s3.listObjects! category is '+category);
-                console.log(data);
+                // console.log(data);
                 for (var i = 0; i < data.KeyCount; i++) {
                     var element = data.Contents[i];
                     var name = element.Key
@@ -56,11 +75,30 @@ module.exports = function (app) {
                 }
                 
             }
-            console.log('\n sounds are ' + JSON.stringify(soundsArray));
+            // console.log('\n sounds are ' + JSON.stringify(soundsArray));
             res.render('pages/addSounds', { sounds: soundsArray, category: category });
         });
-        // res.render('pages/addSounds', { sounds: soundsArray, category: category });
     });
+
+    var sound = function(url, dest, callback){
+        var file = fs.createWriteStream(dest);
+        var request = https.get(url, function(response){
+            response.pipe(file);
+            file.on('finish', function(){
+                file.close(callback);
+            });
+        }).on('error', function(err){
+            fs.unlink(dest);
+            if(callback)
+                callback(err.message);
+        });
+    }
+
+    var soundDownloaded = function(err){
+        console.log('sound has been downloaded and callback function has been called.');
+        if (err)
+            console.error('error is '+err.message);
+    }
 
     app.get('/reviewGIFovie', function (req, res) {
         var gifurls = req.cookies['gifURLs'].split("_");
@@ -68,13 +106,7 @@ module.exports = function (app) {
 
         var soundurls = req.cookies['soundURLs'].split("_");
         // console.log("soundURLs: "+soundurls);
-
-        // var gifurls = "https://media.tenor.com/images/bd05c1611762e65ee614e43f5422f57f/tenor.gif_https://media.tenor.com/images/5b6c59d084c421486b907102b260536f/tenor.gif_null_null".split("_");
-        console.log("gifURLs: "+gifurls);
-
-        // var soundurls = "https://gifovie-bucket.s3.us-west-2.amazonaws.com/sounds/funny/LongBurp.wav?AWSAccessKeyId=AKIAJNY3GON2EE4DM7LA&Expires=1552028173&Signature=N3sASBxioUQWx/P9bgk2eCVHxTU=_https://gifovie-bucket.s3.us-west-2.amazonaws.com/sounds/funny/MaleLaugh.wav?AWSAccessKeyId=AKIAJNY3GON2EE4DM7LA&Expires=1552028173&Signature=GGbk/RqLvazOabQX6HdiLH+nmt0=_null_null".split("_");
-        console.log("soundURLs: "+soundurls);
-
+        
         var gifovieMap = new Map();
 
         gifurls.forEach(function(value, index){
@@ -118,63 +150,115 @@ module.exports = function (app) {
                 });
 
                 soundurls[index] = soundsArray.pop();
-                console.log('sounds: '+soundurls[index]);
+                // console.log('sounds: '+soundurls[index]);
             }
         });
 
         console.log("gifovieMap length: "+gifovieMap.size);
 
-        gifovieMap.forEach(function(value, key){
-            console.log('key is '+key+' value is '+value);
-            console.log('gif url is '+(value[0].includes("tenor") && value[0].includes(".gif") ? value[0] : null));
-            console.log('sound url is '+(value[1].includes("s3") && value[1].includes(".wav") ? value[1] : null));
+        function mergeGIFAndSoundFiles(gifovieMap){
+            return new Promise(function(resolve, reject){
+                var mp4Exists = [];
+                gifovieMap.forEach(function(value, key){
+                        console.log('gif url is '+(value[0].includes("tenor") && value[0].includes(".gif") ? value[0] : null));
+                        console.log('sound url is '+(value[1].includes("s3") && value[1].includes(".wav") ? value[1] : null));
+                        
+                        var s = sound(value[1], key+'.wav', soundDownloaded);
+            
+                        ffmpeg()
+                        .on('start', function(){
+                            console.log("ffmpeg spawned!");
+                        })
+                        .addInput(value[0].includes("tenor") && value[0].includes(".gif") ? value[0] : null)
+                        // .addInput(value[1].includes("s3") && value[1].includes(".wav") ? value[1] : null)
+                        // .addInput('car-horn.wav')
+                        .addInput(key+'.wav')
+                        .on('progress', function(progress){
+                            console.log("processing!");
+                        })
+                        .on('end', function(){
+                            console.log("merge process with ffmpeg ended!")
+                        })
+                        .duration(3)
+                        .saveToFile(key+'.mp4', function(stdout, stderr){
+                            console.log("File combined and saved successfully!");
+                            mp4Exists.push(key+'.mp4');
+                        });
+                    });
 
-            ffmpeg()
-            .on('start', function(){
-                console.log("ffmpeg spawned!");
-            })
-            .addInput(value[0].includes("tenor") && value[0].includes(".gif") ? value[0] : null)
-            //.addInput(value[1].includes("s3") && value[1].includes(".wav") ? value[1] : null)
-            .addInput('car-horn.wav')
-            .on('progress', function(progress){
-                console.log("processing!");
-            })
-            .on('end', function(){
-                console.log("merge process with ffmpeg ended!")
-            })
-            .duration(3)
-            .saveToFile(key+'.mp4', function(stdout, stderr){
-                console.log("File combined and saved successfully!");
+                mp4Exists.forEach(function(value, key){
+                    if(fs.exists(value))
+                        resolve(value+' has been created!');
+                    else
+                        reject('merging gif with sound failed for '+key);
+                });
             });
-        });
+        }
+
+        mergeGIFAndSoundFiles(gifovieMap)
+        .then(function(){
+            console.log("Called after promise!");
+        }).catch(function(err){
+            console.log("Catching error! Error: "+err);
+        })
+
+        // gifovieMap.forEach(function(value, key){
+        //     // console.log('key is '+key+' value is '+value);
+        //     console.log('gif url is '+(value[0].includes("tenor") && value[0].includes(".gif") ? value[0] : null));
+        //     console.log('sound url is '+(value[1].includes("s3") && value[1].includes(".wav") ? value[1] : null));
+            
+        //     var s = sound(value[1], key+'.wav', soundDownloaded);
+
+        //     ffmpeg()
+        //     .on('start', function(){
+        //         console.log("ffmpeg spawned!");
+        //     })
+        //     .addInput(value[0].includes("tenor") && value[0].includes(".gif") ? value[0] : null)
+        //     // .addInput(value[1].includes("s3") && value[1].includes(".wav") ? value[1] : null)
+        //     // .addInput('car-horn.wav')
+        //     .addInput(key+'.wav')
+        //     .on('progress', function(progress){
+        //         console.log("processing!");
+        //     })
+        //     .on('end', function(){
+        //         console.log("merge process with ffmpeg ended!")
+        //     })
+        //     .duration(3)
+        //     .saveToFile(key+'.mp4', function(stdout, stderr){
+        //         console.log("File combined and saved successfully!");
+        //     });
+        // });
 
         console.log("Entering gifovie combine phase!");
 
         var fileList = [];
-        // ["gifovie0.mp4", "gifovie1.mp4", "gifovie2.mp4", "gifovie3.mp4"];
         gifovieMap.forEach(function(value, key){
             fileList.push(key+".mp4");
         })
 
         console.log('filelist names: '+fileList);
         
-        var listFileNames = __dirname +"\\"+"files.txt", fileNames = "";
-        fileList.forEach(function(fileName, index){
-            console.log("fileName: "+fileName);
-            fileNames = fileNames + " file " + "'" + fileName + "'\r\n";
-        });
-
-        fs.writeFileSync(listFileNames, fileNames);
-
-        ffmpeg().input(listFileNames)
-        .inputOptions(['-f concat', '-safe 0'])
-        .outputOptions('-c copy')
-        .save('gifovie.mp4');
-
-        for(var key of gifovieMap.keys()){
-            console.log('key is '+key+'; value is '+gifovieMap.get(key));
-        }
+        var listFileNames = process.cwd() +"\\list.txt", fileNames = "";
+        console.log('file to store sub-gifovies: '+listFileNames);
         
+        // fileList.forEach(function(fileName, index){
+        //     console.log("fileName: "+fileName);
+        //     fileNames = fileNames + " file " + "'" + fileName + "'\r\n";
+        // });
+
+        // fs.writeFileSync(listFileNames, fileNames);
+
+        // ffmpeg()
+        // // .on('start', function(){
+        // //     console.log('in ffmpeg combine phase! listFileNames: '+listFileNames);
+        // //     fs.readFile(listFileNames, function(err, data){
+        // //         console.log("content from list.txt is "+data);
+        // //     });
+        // // })
+        // .input('list.txt')
+        // .inputOptions(['-f concat', '-safe 0'])
+        // .outputOptions('-c copy')
+        // .save('gifovie.mp4');
 
         res.render('pages/reviewGIFovie');
     });
